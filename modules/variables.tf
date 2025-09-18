dd18eafc70aa2ac6a7480d7f6f1d1fcd472153a4
Rocio.data <tunidev56@gmail.com>
2025-09-18 19:01:55 -0300

    Create variables.tf

---
+++ modules/variables.tf
@@ -0,0 +1,5 @@
+variable "state_machine_name" { type = string }
+variable "role_arn" { type = string }
+variable "lambda_ingest_arn" { type = string }
+variable "sagemaker_training_arn" { type = string }
+variable "lambda_scoring_arn" { type = string }
